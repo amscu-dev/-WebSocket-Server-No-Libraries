4c436a5d568b3864526ea40030f530517dc60bb6
amscu-dev <amarinescu.dev@gmail.com>
2026-02-25 12:02:04 +0200

    feat(server): add a simple node http server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 import "module-alias/register";
 
+import http from "node:http";
+
 import chalk from "chalk";
 
-import config from "./config/app.config";
+import CONFIG from "@/config/app.config";
 
-console.log(config.PORT);
-console.log(chalk.green.bold("Finished loading all files in index.ts"));
+console.log(
+  chalk.green.bold("[ NODE ] Finished loading all files in index.ts"),
+);
+
+// create a HTTP wev-server object
+const httpServer = http.createServer((request, response) => {
+  // for a request to ws:// , the followind code inside of here will NOT be executed, instead the request will be passed onto the upgrade event emitter
+  // if no 'upgrade' event listener, an error will be thrown
+  response.writeHead(200);
+  response.end("OK");
+});
+
+// start HTTP server
+httpServer.listen(CONFIG.PORT, CONFIG.HOST, () => {
+  console.log(
+    chalk.green.bold(
+      `[ NODE ] Http server started on port ${CONFIG.PORT} and host ${CONFIG.HOST}`,
+    ),
+  );
+});
